ca5643a790a7aeaa168b626604c60aec9693e76e
yashsharma954 <yashsharmarohit21@gmail.com>
2026-06-30 09:49:16 +0530

    fix: remove next from async pre-save hook

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,11 +54,9 @@ const userSchema = new Schema({
 });
 
 // Password hashing
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 });
 
 // Methods
